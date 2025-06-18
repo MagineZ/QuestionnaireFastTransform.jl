dc62e783314caf2c4b88b8411779f29345b033fa
Documenter.jl <documenter@juliadocs.github.io>
2025-06-18 14:49:53 +0000

    build based on fade2e4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#QuestionnaireFastTransform","page":"Home","title":"QuestionnaireFastTransform","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuestionnaireFastTransform.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
